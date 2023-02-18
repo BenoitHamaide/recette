84ad9a7b9df20cd20c339f2e45fb13800313b6bd
Benoit HAMAIDE <hamaidebenoit@gmail.com>
2023-02-18 13:11:36 +0100

    data 2 fait

---
+++ src/data/RecipeAnother.js
@@ -0,0 +1,54 @@
+export default {
+  title: 'Pancakes facile et rapide',
+  thumbnail: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  author: 'Benoît',
+  difficulty: 'Moyen',
+  ingredients: [
+    {
+      id: 1,
+      quantity: 1,
+      unit: 'cuillère à soupe',
+      name: 'de sucre blanc',
+    },
+    {
+      id: 2,
+      quantity: 1,
+      unit: 'cuillère à soupe ',
+      name: 'd\'huile',
+    },
+    {
+      id: 15,
+      quantity: 150,
+      unit: 'g',
+      name: 'de farine',
+    },
+    {
+      id: 28,
+      quantity: 1,
+      unit: 'cuillère à café',
+      name: 'de levure',
+    },
+    {
+      id: 4,
+      quantity: 1,
+      unit: '',
+      name: 'oeuf',
+    },
+    {
+      id: 3,
+      quantity: 200,
+      unit: 'ml',
+      name: 'de lait',
+    },
+  ],
+  instructions: [
+    'Battre grossièrement l\'oeuf et le sucre dans un saladier',
+    'Y rajouter le beurre fondu ou l\'huile',
+    'Mélanger à part la farine et la levure, puis en mélanger la moitié avec la préparation',
+    'Délayer progressivement avec le lait tout en rajoutant l\'autre moitié de farine petit à petit (cela évitera de créer des grumeaux)',
+    'Aucun temps de repos n\'est nécessaire, si ce n\'est juste le temps de faire chauffer votre poêle à feu moyen. Huilez ou beurrez la poêle à l\'aide de papier essuie-tout',
+    'A l\'aide d\'une petite louche ou d\'une grosse cuillère à sauce, déposer des "ronds" dans la poêle',
+    'Lorsque les bulles apparaissent et éclatent, retourner les pancakes et ne pas les laisser plus d\'une min sur l\'autre face',
+    'Dégustez !',
+  ],
+};
